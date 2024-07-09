b331822fb5a4d220c777becebc75a0f98490265e
reza1mahb <136892089+reza1mahb@users.noreply.github.com>
2024-07-09 10:59:00 +0330

    Update changes.sh

--- changes.sh
+++ changes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Requires wkhtmltopdf and aha tools
 # Install using: sudo apt install wkhtmltopdf aha
-git diff --color upstream-v0.7.4-rc.1 | aha > CHANGES.html && wkhtmltopdf CHANGES.html CHANGES.pdf && rm CHANGES.html
+git diff --color upstream-v0.8.9-rc.1 | aha > CHANGES.html && wkhtmltopdf CHANGES.html CHANGES.pdf && rm CHANGES.html
